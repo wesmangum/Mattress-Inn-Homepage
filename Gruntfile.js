d72d0c248f6444b15b5b01c3864d5d9a654335af
wesmangum <wesmangum24@gmail.com>
2015-05-23 16:07:04 -0500

    initial commit v2

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          "style.css": "less/bootstrap.less" // destination file and source file
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['less/**/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  // Default task(s).
+  grunt.registerTask('default', ['less', 'watch'] );
+
+};
